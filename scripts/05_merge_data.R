122107a87ec474fbf7cf1f355feb37f29c9cf0f2
Abhishek Bhatia <abhishekbhatia08@gmail.com>
2022-11-08 11:52:16 -0500

    🎨 Improved structure of analytic_df

--- scripts/05_merge_data.R
+++ scripts/05_merge_data.R
@@ -51,6 +51,31 @@ analytic_df <- analytic_df %>% left_join(pop_df, by = c("fips"))
 analytic_df <-
   analytic_df %>% left_join(asthma_df, by = c("year", "name"))
 
-## 3. Write to directory ----
+## 3. Sort Columns ----
+analytic_df <-
+  analytic_df %>% dplyr::select(
+    year,
+    fips,
+    name,
+    ed_rate,
+    mean_days,
+    burned_ratio,
+    mean_intensity,
+    mean_aerosol,
+    n_pop_2018,
+    p_hosp_permil,
+    p_bedsper1000,
+    p_mds1000,
+    p_medicare_bene1000,
+    p_medicare_elig1000,
+    p_poverty,
+    p5younger,
+    p65older,
+    p_nonwhite
+  )
+
+
+
+## 4. Write to directory ----
 readr::write_csv(analytic_df,
                  here::here("derived_data", "analytic_df.csv"))
